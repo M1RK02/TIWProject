08cee3737c9c8113d85f0938078a315ad78c47f0
M1RK02 <picamirko02@gmail.com>
2024-07-12 18:37:03 +0200

    Started Javascript version

--- Javascript/src/main/java/controllers/RegisterUser.java
+++ Javascript/src/main/java/controllers/RegisterUser.java
@@ -6,27 +6,24 @@ import java.sql.SQLException;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.MultipartConfig;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang.StringEscapeUtils;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
-import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 import dao.UserDAO;
 import utils.ConnectionHandler;
 
 @WebServlet("/RegisterUser")
+@MultipartConfig
 public class RegisterUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Connection connection = null;
-	private TemplateEngine templateEngine;
 	
 	private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
 
@@ -35,12 +32,6 @@ public class RegisterUser extends HttpServlet {
 	}
 
 	public void init() throws ServletException {
-		ServletContext servletContext = getServletContext();
-		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
-		templateResolver.setTemplateMode(TemplateMode.HTML);
-		this.templateEngine = new TemplateEngine();
-		this.templateEngine.setTemplateResolver(templateResolver);
-		templateResolver.setSuffix(".html");
 		connection = ConnectionHandler.getConnection(getServletContext());
 	}
 
@@ -52,49 +43,43 @@ public class RegisterUser extends HttpServlet {
 		String email = null;
 		String name = null;
 		String surname = null;
-		try {
-			username = StringEscapeUtils.escapeJava(request.getParameter("username"));
-			password = StringEscapeUtils.escapeJava(request.getParameter("password"));
-			repeatPassword = StringEscapeUtils.escapeJava(request.getParameter("repeatPassword"));
-			email = StringEscapeUtils.escapeJava(request.getParameter("email"));
-			name = StringEscapeUtils.escapeJava(request.getParameter("name"));
-			surname = StringEscapeUtils.escapeJava(request.getParameter("surname"));
-			if (Stream.of(username, password, repeatPassword, email, name, surname).anyMatch(str -> str == null || str.isEmpty())) {
-				throw new Exception("Missing or empty credential value");
-			}
-		} catch (Exception e) {
-			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing credential value");
+		username = StringEscapeUtils.escapeJava(request.getParameter("username"));
+		password = StringEscapeUtils.escapeJava(request.getParameter("password"));
+		repeatPassword = StringEscapeUtils.escapeJava(request.getParameter("repeatPassword"));
+		email = StringEscapeUtils.escapeJava(request.getParameter("email"));
+		name = StringEscapeUtils.escapeJava(request.getParameter("name"));
+		surname = StringEscapeUtils.escapeJava(request.getParameter("surname"));
+		if (Stream.of(username, password, repeatPassword, email, name, surname).anyMatch(str -> str == null || str.isEmpty())) {
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.getWriter().println("Missing or empty credential value");
 			return;
 		}
-		
-		ServletContext servletContext = getServletContext();
-		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-		String path = "/index.html";
 
 		// Check if username is unique
 		UserDAO userDao = new UserDAO(connection);
 		try {
 			if(!userDao.isUsernameUnique(username)){
-				ctx.setVariable("errorMsg", "Username not unique");
-				templateEngine.process(path, ctx, response.getWriter());
+				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+				response.getWriter().println("Username not unique");
 				return;
 			}
 		} catch (SQLException e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to check username uniqueness");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().println("Not Possible to check credentials");
 			return;
 		}
 		
 		// Check if repeatPassword matches password
 		if (!repeatPassword.equals(password)) {
-			ctx.setVariable("errorMsg", "Field Repeat Password does not match Password");
-			templateEngine.process(path, ctx, response.getWriter());
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.getWriter().println("Field Repeat Password does not match Password");
 			return;
 		}
 		
 		// Check if email is valid
 		if (!Pattern.compile(EMAIL_REGEX).matcher(email).matches()) {
-			ctx.setVariable("errorMsg", "Email is not valid");
-			templateEngine.process(path, ctx, response.getWriter());
+			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			response.getWriter().println("Email is not valid");
 			return;
 		}
 		
@@ -102,12 +87,13 @@ public class RegisterUser extends HttpServlet {
 		try {
 			userDao.addUser(username, password, email, name, surname);
 		} catch (SQLException e) {
-			response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Not possible to register user");
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			response.getWriter().println("Not Possible to check credentials");
 			return;
 		}
-
-		ctx.setVariable("genericMsg", "User registered successfully");
-		templateEngine.process(path, ctx, response.getWriter());
+		
+		response.setStatus(HttpServletResponse.SC_OK);
+		response.getWriter().println("User registered successfully");
 	}
 
 	public void destroy() {
---
+++ Javascript/src/main/webapp/credentialManager.js
@@ -0,0 +1,67 @@
+/**
+ * Credential Manager
+ */
+
+(function() { // avoid variables ending up in the global scope
+
+  document.getElementById("loginButton").addEventListener('click', (e) => {
+    var form = e.target.closest("form");
+    if (form.checkValidity()) {
+      makeCall("POST", 'CheckLogin', e.target.closest("form"),
+        function(x) {
+          if (x.readyState == XMLHttpRequest.DONE) {
+            var message = x.responseText;
+            switch (x.status) {
+              case 200:
+            	sessionStorage.setItem('username', message);
+                window.location.href = "home.html";
+                break;
+              case 400: // bad request
+                document.getElementById("errorMessage").textContent = message;
+                break;
+              case 401: // unauthorized
+                  document.getElementById("errorMessage").textContent = message;
+                  break;
+              case 500: // server error
+            	document.getElementById("errorMessage").textContent = message;
+                break;
+            }
+          }
+        }
+      );
+    } else {
+    	 form.reportValidity();
+    }
+  });
+  
+    document.getElementById("signUpButton").addEventListener('click', (e) => {
+    var form = e.target.closest("form");
+    if (form.checkValidity()) {
+      makeCall("POST", 'RegisterUser', e.target.closest("form"),
+        function(x) {
+          if (x.readyState == XMLHttpRequest.DONE) {
+            var message = x.responseText;
+            switch (x.status) {
+              case 200:
+            	document.getElementById("genericMessage").textContent = message;
+            	document.getElementById("errorMessage").textContent = "";
+                break;
+              case 400: // bad request
+                document.getElementById("errorMessage").textContent = message;
+                break;
+              case 401: // unauthorized
+                  document.getElementById("errorMessage").textContent = message;
+                  break;
+              case 500: // server error
+            	document.getElementById("errorMessage").textContent = message;
+                break;
+            }
+          }
+        }
+      );
+    } else {
+    	 form.reportValidity();
+    }
+  });
+
+})();
